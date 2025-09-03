dd42de394bc69e6fc7777b6583297026dc36fbb1
newe08001 <newe08001@E-PW05SJ7Q>
2025-09-03 13:02:15 +0200

    Add project files.

---
+++ Upgift 1,2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Upggift1._2
+
+
+{ class Program
+    { static void Main(string[]args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("A problem has been detected and Windows has been shut down to prevent damage to your computer.\r\n\r\nIf this is the first time you've seen this stop error screen, restart your computer. if this screen appears again, follow these steps:\r\n\r\nCheck to make sure any new hardware or software is properly installed. I this is a new installation, ask your hardware or software manufacturer for and Windows updates you might need.\r\n\r\nIf problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.\r\n\r\nTechnical information:\r\n\r\n*** STOP: 0x000000FE (0x00000008, 0x000000006, 0x00000009, 0x847075cc)");
+            Console.ReadKey();
+
+        }
+        
+            }
+ 
+}
